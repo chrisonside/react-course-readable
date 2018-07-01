ff0d01dfeab1688484507c84bf5c363f37aac732
Chris Reidy <christopher.t.reidy@googlemail.com>
2018-07-02 00:28:51 +0100

    Remove filter reducer from master reducer

--- src/reducers/masterReducer.js
+++ src/reducers/masterReducer.js
@@ -3,14 +3,12 @@ import { combineReducers } from 'redux';
 // Import reducers
 import { categories } from './categories';
 import { comments } from './comments';
-// import { filter } from './filter';
 import { posts } from './posts';
 import { votes } from './votes';
 
 export default combineReducers({
   categories,
   comments,
-  // filter,
   posts,
   votes
 });
